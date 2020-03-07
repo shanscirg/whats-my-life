583e5b9673e9f2720c6f82aae7a162558b0d6605
Shannon Scirghio <shanscirg7@gmail.com>
2020-03-07 15:06:45 -0700

    started results js, questions handlebars and routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const router = express.Router();
-// const fs = require("fs");
-const results = require("../personalities/personality")
+const results = require("../personalities/personality");
+const db = require("../models");
 
 router.get("/", (req, res) => res.send("TESTING"));
+
+router.get("/test", function (req, res) {
+    db.Question.findAll({})
+        .then(function (data) {
+            // const question = data[0].Question.dataValues;
+            // console.log(data[0.Question);
+            // console.log(data[0]);
+            const questionObj = {values: []}; 
+            data.map(value => questionObj.values.push(value.dataValues))
+            // console.log(questionObj);
+            // console.log(hbsObject.questions[0].dataValues);
+            res.render("questions", questionObj);
+        });
+});
 
 router.get("/results/:result", (req, res) => {
     const personality = results.filter(item => item.type === req.params.result.toUpperCase())
