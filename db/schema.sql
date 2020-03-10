7ac6c99f7ac426a6a65c0a69b62ab8508bf3b765
ljv29 <jumpswervelogan@gmail.com>
2020-03-10 00:12:58 -0700

    modified schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,17 @@
-### Schema
+create database lads_db;
 
-DROP DATABASE IF EXISTS lads_db;
-CREATE DATABASE lads_db;
-USE lads_db;
+use lads_db;
+
+create table questions(
+    id int NOT NULL AUTO_INCREMENT,
+	question varchar(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+create table user(
+id int NOT NULL auto_increment,
+email varchar(40),
+password varchar(40),
+results varchar(40),
+PRIMARY KEY (id)
+);
